62a725aad6823ad0e428ea8a2418a9b43325d6c3
MANU4141 <jhjung0401@gmail.com>
2024-07-10 18:10:01 +0900

    Update 1546.c

--- class 2/1546.c
+++ class 2/1546.c
@@ -8,20 +8,25 @@ Language: C 99
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
 	int n, score;
 	int max = 0;
 	int total = 0;
 
 	scanf("%d", &n);
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i) 
+	{
 		scanf("%d", &score);
 		total += score;
 		
-		if (score > max) {
+		if (score > max)
+		{
 			max = score;
 		}
 	}
 	printf("%f\n", 100.0 * total / n / max);
+	
+	return 0;
 }
